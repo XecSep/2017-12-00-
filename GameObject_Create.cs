0859dfb1ca877d260c8e3298bf3840da409e2ff1
XecSep <kij4300@naver.com>
2017-12-12 19:09:21 +0900

    Update GameObject_Create.cs

--- GameObject_Create.cs
+++ GameObject_Create.cs
@@ -1,23 +1,30 @@
 # 2017-12-00-
 2017-12-00에 만든 유니티 게임 프로젝트입니다.
 
-//게임오브젝트를 만든다.
+//게임오브젝트를 만들고, 컴포넌트를 삽입한다.
 
 using UnityEngine;
 using System.Collections;
-
-public class GameObject_Create : MonoBehaviour {
-
-	// Use this for initialization
-	void Start ()
-    	{
-        	GameObject Player;
-        	Player = new GameObject("Player");
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    	{
+ 
+public class Test : MonoBehaviour {
+ 
+    // Use this for initialization
+    void Start ()
+    {
+	//게임오브젝트를 만든다
+        GameObject Player;	
+        Player = new GameObject("Player");
+	    
+	//컴포넌트를 삽입한다.
+        Player.AddComponent<MeshFilter>();	
+        Player.AddComponent<BoxCollider>();
+        Player.AddComponent<MeshRenderer>();
+    }
     
-    	}
+    // Update is called once per frame
+    void Update ()
+    {
+        
+    }
 }
+
