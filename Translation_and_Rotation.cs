5e2dc5664472b98c180f9e2d192e6cfe1635477a
XecSep <kij4300@naver.com>
2017-12-12 19:27:52 +0900

    Create Translation_and_Rotation.cs

---
+++ Translation_and_Rotation.cs
@@ -0,0 +1,33 @@
+# 2017-12-00-
+2017-12-00에 만든 유니티 게임 프로젝트입니다.
+
+//↔회전한다. ↕움직인다.
+
+using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+    public float speed = 10.0F;
+    public float rotationSpeed = 100.0F;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;  //translation 이동
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed; //rotation 회전
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+    }
+}
