96909187f728f1ba71d2d00b585185b0e445f227
XecSep <kij4300@naver.com>
2017-12-14 11:17:37 +0900

    Update Player_BasicTransform.cs

--- Player_BasicTransform.cs
+++ Player_BasicTransform.cs
@@ -17,7 +17,7 @@ public class Player_BasicTransform. : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-      transform.position = new Vector3(1, 1, 1);	//Transform > Position값을 변경한다.
+      transform.position = new Vector3(1, 1, 1);	//Transform > Position값을 변경한다. //오브젝트를 움직이지는 못한다.오브젝트를 움직이려면 transform.Translate()함수를 써야한다.
       transform.rotation = Quaternion.LookRotation(Vector3.back);	//Transform > Rotation값을 변경한다.
       transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);	//Transform > Scale값을 변경한다.
     }
