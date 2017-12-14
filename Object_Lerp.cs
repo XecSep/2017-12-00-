4f0b8853a2ec2e78a1f8028f42ba49e828025b7f
XecSep <kij4300@naver.com>
2017-12-14 12:20:47 +0900

    Create Object_Lerp.cs

---
+++ Object_Lerp.cs
@@ -0,0 +1,24 @@
+# 2017-12-00-
+2017-12-00에 만든 유니티 게임 프로젝트입니다.
+
+//Quaternion.Lerp()
+
+using UnityEngine;
+using System.Collections;
+
+public class Object_Lerp : MonoBehaviour {
+
+    public GameObject a;
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+      transform.rotation = Quaternion.Lerp(transform.rotation, a.transform.rotation, 0.1f);
+    }
+}
