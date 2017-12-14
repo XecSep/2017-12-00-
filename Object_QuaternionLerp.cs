9dffb0eb86bc1ec01fbcc3ed7c840311587f49df
XecSep <kij4300@naver.com>
2017-12-14 12:38:33 +0900

    Update Object_QuaternionLerp.cs

--- Object_QuaternionLerp.cs
+++ Object_QuaternionLerp.cs
@@ -19,6 +19,6 @@ public class Object_QuaternionLerp : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-      transform.rotation = Quaternion.Lerp(transform.rotation, a.transform.rotation, 0.1f);
+      transform.rotation = Quaternion.Lerp(transform.rotation, a.transform.rotation, 0.1f);	//Lerp(a,b,t)이다.a를 b까지 t시간만큼 비례하여 바꾼다. t는 0~1값을 입력한다.최대값1을 입력하면 a는 한번에 b가 된다.중간값0.5f를 입력하면 a는 0.5시간동안 b가 된다. 일반적으로 a = ~Lerp(a,b,t)형태로 사용한다.
     }
 }
