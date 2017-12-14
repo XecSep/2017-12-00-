f14d7aa82ac0f65acc0ebec19499101827dc3681
XecSep <kij4300@naver.com>
2017-12-14 10:58:45 +0900

    Update Player_position.cs

--- Player_position.cs
+++ Player_position.cs
@@ -13,7 +13,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class Vector3 : MonoBehaviour {
+public class Player_position : MonoBehaviour {
 
     // Use this for initialization
     void Start ()
