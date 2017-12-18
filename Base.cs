93fafc2d782aede62bd09655da7796583ddfd2bc
XecSep <kij4300@naver.com>
2017-12-18 13:55:15 +0900

    Create Base.cs

---
+++ Base.cs
@@ -0,0 +1,20 @@
+# 2017-12-00-
+2017-12-00에 만든 유니티 게임 프로젝트입니다.
+
+using UnityEngine;
+using System.Collections;
+
+public class Base : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
