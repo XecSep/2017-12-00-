914f5d80f7e2e68839727ca405aebeea3e6234c8
XecSep <kij4300@naver.com>
2017-12-18 13:57:54 +0900

    Create Vector_position.cs

---
+++ Vector_position.cs
@@ -0,0 +1,21 @@
+# 2017-12-00-
+2017-12-00에 만든 유니티 게임 프로젝트입니다.
+
+using UnityEngine;
+using System.Collections;
+
+public class Vector_position : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Vector3 dir = new Vector3(1, 1, 1);
+        transform.position = dir;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
